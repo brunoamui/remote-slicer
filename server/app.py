39838b7059af83e5b3f51156378af22ad61dbdc7
brunoamui <brunoamui@gmail.com>
2016-10-07 23:26:05 -0300

    cross origins

--- server/app.py
+++ server/app.py
@@ -9,9 +9,11 @@ import redis
 from rq import Queue
 import server
 import uuid
+from flask_cors import CORS, cross_origin
 
 # Initialize the Flask application
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 conn = redis.from_url("redis://redistogo:436473da6c8d57832bbf8ac3235490a0@sculpin.redistogo.com:10283")
 q = Queue(connection=conn)
