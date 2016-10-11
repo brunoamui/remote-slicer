7733bf11cc9a52d8db81fe3e144fdcce20479d8e
brunoamui <brunoamui@gmail.com>
2016-10-11 12:38:38 -0300

    muitas conexoes no redis, lembrar do worker

--- server/worker.sh
+++ server/worker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ../venv/bin/activate
 #rq worker -u redis://redistogo:436473da6c8d57832bbf8ac3235490a0@sculpin.redistogo.com:10283
-rq worker -u redis://:passtest@redis-10033.c10.us-east-1-2.ec2.cloud.redislabs.com:10033/0
+rq worker -u redis://:passtest@redis-15555.c10.us-east-1-2.ec2.cloud.redislabs.com:15555/0
